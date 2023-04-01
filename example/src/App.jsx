f10b00a9b288db7b3e6d8b6f850518a8dfd4278e
Hein Pieterse <hp.pieterse@gmail.com>
2023-04-01 10:05:24 +0200

    feat: add simultaneous update example

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -5,24 +5,37 @@ function App() {
   const [count, setCount] = useUrlQueryState('c', 0)
   const [text, setText] = useUrlQueryState("s", "");
   const [data, setData] = useUrlQueryState("d", { name: "name", value: 0 });
+
+  const clearAllHandler = () => {
+    setCount(0);
+    setText("");
+    setData({ name: "name", value: 0 });
+  }
+
+  const updateAllHandler = () => {
+    setCount(count + 1);
+    setText(`Value ${(Math.random() + 1).toString(36).substring(7)}`);
+    setData({ ...data, value: data.value + 1 })
+
+  }
   return (
     <div className="App">
       <h1>useUrlQueryState</h1>
       <p>Update the values below and see how the route query parameter updated</p>
       <div className="card">
-        <div>
-          <input value={text} onInput={(e) => setText(e.target.value)} placeholder="Enter value"></input>
-        </div>
-        <div>
-          <button onClick={() => setCount(count + 1)}>
-            count is {count}
-          </button>
-        </div>
-        <div>
-          <button onClick={() => setData({ ...data, value: data.value + 1 })}>
-            Update object value {JSON.stringify(data)}
-          </button>
-        </div>
+        <input value={text} onInput={(e) => setText(e.target.value)} placeholder="Enter value"></input>
+        <button onClick={() => setCount(count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setData({ ...data, value: data.value + 1 })}>
+          Update object value {JSON.stringify(data)}
+        </button>
+        <button onClick={clearAllHandler}>
+          Clear all
+        </button>
+        <button onClick={updateAllHandler}>
+          Update all values at once
+        </button>
       </div>
       <a className="read-the-docs" href="https://github.com/hpieterse/use-url-query-state">
         View the documentation
